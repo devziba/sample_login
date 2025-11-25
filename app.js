552bb4c8a3cbe65a8cbaceaca2a8da3ce54ab06d
devziba <devwork64@gmail.com>
2025-11-25 20:02:38 +0530

    updated code

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const jwt = require('jsonwebtoken') 
 const bcrypt = require('bcrypt')
+const user = require('./models/user')
 
 
 app.set('view engine','ejs')
@@ -13,9 +14,23 @@ app.use(express.static(path.join(__dirname,'public')))
 app.use(express.json())
 
 
-app.get('/login',(req,res)=>{
-    
+app.post('/register',async (req,res)=>{
+    let myUser = await user.findOne({email: req.body.email})
+    if(!myUser){
+        let salt = await bcrypt.genSalt(10)
+        let hash = await bcrypt.hash(req.body.password,salt)
+       let createdUser = await user.create({
+            username: req.body.username,
+            email:req.body.email,
+            password: hash,
+        })
+        let token = jwt.sign({email:createdUser.email,id:createdUser._id},'secret')
+        res.cookie('token',token)
+        res.json({message:'done'})
+    }else{
+    res.json({message:'not_done'})
+    }
 })
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT)
