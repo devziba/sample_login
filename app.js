924ce0236b2567a733c4dd67728aeeeb9a76626c
devziba <devwork64@gmail.com>
2025-11-25 14:43:40 +0530

    nothing two

--- app.js
+++ app.js
@@ -17,27 +17,6 @@ app.use(express.json())
 app.get('/register',async (req,res)=>{
     res.send('hello')
 })
-app.post('/register',async (req,res)=>{
-    try{
-    let salt = await bcrypt.genSalt(10)
-    let hash = await bcrypt.hash(req.body.password,salt)
-
-    let createdUser= await user.create({
-        name:req.body.name,
-        email:req.body.email,
-        password: hash
-    })
-    let token = jwt.sign({email:createdUser.email,id:createdUser._id},'secret')
-    res.cookie('token',token,{
-        httpOnly:true,
-        secure:true
-    })
-    res.status(200).json({message:'okay'})
-} catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Something went wrong" });
-    }
-})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
